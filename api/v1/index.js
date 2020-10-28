69f1156ba19b0631879da610edec467689086f90
Jonathan Pipard <jonathan.pipard@gmail.com>
2020-10-28 16:27:54 +0100

    Working on 28/10/20

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,11 +1,55 @@
-const express = require("express")
+const express = require('express');
+const router = express.Router(); // Création d"un router
 
-const router = express.Router() // Création d"un router
+const Blogpost = require('../models/blogpost');
+
 
 // Le router ne reagira que si on fait une requète /ping
-router.get("/ping", (req, res) => {
-	res.status(200).json({ msg: "pong", date: new Date() })
-})//localhost:3000/ping
+router.get('/ping', (req, res) => {
+	res.status(200).json({ msg: 'pong', date: new Date() });
+});//localhost:3000/ping
 
-module.exports = router
+router.get('/blog-posts', (req, res) => {
+	const posts = [
+		{
+			id: 1,
+			title: 'Etre programmeur ça pique parfois !!!',
+			subTitle: 'ce cactus est pas là pour rien',
+			image: 'https://cdn.vuetifyjs.com/images/cards/sunshine.jpg',
+			content:
+        'I\'m a thing. But, like most politicians, he promised more than he could deliver. You won\'t have time for sleeping, soldier. not with all the bed making you\'ll be doing. Then we\'ll go with that data file! Hey, you add a one and two zeros to that or we walk! You\'re going to do his laundry? I\'ve got to find a way to escape.',
+		},
+		{
+			id: 2,
+			title: 'Un nouveau framework JS',
+			subTitle: 'encore !?',
+			image: 'https://fakeimg.pl/300/?text=frameworks',
+			content: 'Ca en fait 1 par semaine. On va pas tenir le rythme',
+		},
+		{
+			id: 3,
+			title: 'Vive le vanilla JavaScript',
+			subTitle: 'encore !?',
+			image: 'https://fakeimg.pl/300/?text=JS',
+			content: 'Là au moins, on est sûr que ça va durer',
+		},
+	];
+	res.status(200).json(posts);
+
+} );
+
+router.post('/blog-posts', (req, res) => {
+	console.log('req.body', req.body);
+	const blogPost = new Blogpost(req.body);
+	blogPost.save((err, blogPost) => {
+		if (err) {
+			return res.status(500).json(err);
+		}
+		res.status(200).json(blogPost);
+	});
+});
+
+module.exports = router;
+
+
 
--- app.js
+++ app.js
@@ -1,15 +1,39 @@
-const express = require("express")
-const app = express()
-const api = require("./api/v1/index") // permet d'importer router
-const cors = require("cors")
+const express = require('express');
+const app = express();
+const api = require('./api/v1/'); // permet d'importer router
+const cors = require('cors');
 
-app.set("port", (process.env.port || 3000 ))
+
+const mongoose = require('mongoose');
+const connection = mongoose.connection;
+
+app.set('port', (process.env.port || 3000 ));
 
 //Utilisation de cors middleware pour la gestion des requetes
-app.use(cors()) //app.use permet d'utiliser des middleware
-app.use("/api/v1", api) //requete sur localhost:3000/api/v1
 
-// On écoute sur le port défini précedement 
-app.listen(app.get("port"), () => {
-	console.log(`express server listening on port ${app.get("port")}`)
-}) 
+app.use(cors()); //app.use permet d'utiliser des middleware
+app.use('/api/v1', api); //requete sur localhost:3000/api/v1
+app.use((req, res) => {
+	const err = new Error('Erreur 404 aie aie aie');
+	err.status = 404;
+	res.json({ msg:'404 not foung', err:err});
+});
+
+//gestion de la connection à mongoDB
+mongoose.connect('mongodb://localhost:27017/whiskycms',{ useNewUrlParser:true, useUnifiedTopology:true});
+connection.on('error', (err) => {
+	console.error(`connection to MongoDB erro: ${err.message}`);
+});
+
+connection.once('open', () => {
+	console.log('connected to mongoDB');
+
+	// On écoute sur le port défini précedement 
+	app.listen(app.get('port'), () => {
+		console.log(`The express server listening on port ${app.get('port')}`);
+	}); 
+
+});
+
+
+
